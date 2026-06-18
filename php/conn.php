bb4cb75205399c9dab79c30f457178cb4daae8ac
Moumouni <moumounibanao65@gmail.com>
2026-06-18 08:35:59 +0000

    Sécurité: mot de passe MySQL changé + protection conn.php

--- php/conn.php
+++ php/conn.php
@@ -1,14 +1,17 @@
 <?php
+if (basename($_SERVER['SCRIPT_FILENAME']) === 'conn.php') {
+    header('HTTP/1.0 403 Forbidden');
+    die('Accès interdit.');
+}
+
 $host     = 'localhost';
 $login    = 'burkina_user';
-$password = 'burkina2026';
+$password = '1234';
 $database = 'burkina_db';
 
 $conn = mysqli_connect($host, $login, $password, $database);
-
 if (!$conn) {
     die('Erreur de connexion : ' . mysqli_connect_error());
 }
-
 mysqli_set_charset($conn, 'utf8mb4');
 ?>
